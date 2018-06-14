06c5784a3ed70db7f1fd10d15bcc2628a2b9121e
Rafael Rossi <rossirafael@yahoo.com>
2018-06-14 09:17:21 +0200

    Optimisation in combineResults.

--- gumbel/combine.c
+++ gumbel/combine.c
@@ -1,45 +1,46 @@
+/*
+Combine batch results from the rlc package into one results file, typically results.txt.
+
+The C version is 2x faster than Python.
+
+This unbuffered version is faster than the buffered version.
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h> 
 #include <time.h>
 
 #define LINE_SIZE 300
-#define BUFFER_SZ 5000*LINE_SIZE
 
 void combine(char *fname) {
     DIR *d;
-    FILE * fp;
-    char line[LINE_SIZE];
-    char buffer[BUFFER_SZ];
-    short flagHeader = 1;
-    buffer[0] = '\0';  // need to init buffer befroe strcat to it
-    
+    FILE *fin;
+    FILE *fout;
     struct dirent *dir;
+    char header[LINE_SIZE];
+    char values[LINE_SIZE];
+    short flagHeader = 1;
+    
+    fout = fopen(fname, "wb");
     chdir("runs");
     d = opendir(".");
     if (d) {
         while ((dir = readdir(d)) != NULL) {
             if ((strstr(dir->d_name, "Hs")) && (strstr(dir->d_name, ".txt")) ) {
-                //printf("%s\n", dir->d_name);
-                fp = fopen (dir->d_name, "r");
-                fgets(line, LINE_SIZE, fp);  // read first line
+                fin = fopen (dir->d_name, "rb");
+                fgets(header, LINE_SIZE, fin);  // read header
+                fgets(values, LINE_SIZE, fin);  // read values
                 if (flagHeader) {  // append it to buffer only once
-                    strcat(buffer, line);
-                    //printf("  Header is: %s", line);
                     flagHeader = 0;
+                    fputs(header, fout);
                 }
-                fgets(line, LINE_SIZE, fp);  // read second line
-                strcat(buffer, line);
-                //printf("  Line is: %s", line);
-                fclose(fp);
+                fputs(values, fout);
+                fclose(fin);
             }
         }
         closedir(d);
-        chdir("..");
-        //printf("Buffer is: %s", buffer);
-        fp = fopen(fname, "w");
-        fprintf(fp, buffer);
-        fclose(fp);
+        fclose(fout);
     }
 }
 
